1a5ab5f68b697b6a9c9d4b3aafec472b9fe748e1
miguelfriasd <miguelangelfriasduran@gmail.com>
2023-05-13 20:47:35 -0700

    cambios

--- src/Servidor/Servidor.java
+++ src/Servidor/Servidor.java
@@ -4,32 +4,58 @@
  */
 package Servidor;
 
+import Controlador.ControladorUnirsePartida;
+import Logica.ControlPartida;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
  * @author mig_2
  */
-public class Servidor {
+public class ServidorThread implements Runnable{
     
     private final ServerSocket socketServidor;
+    private final ControlPartida controlPartida;
+    private List<ClientHandler> listaClientes;
 
-    public Servidor(ServerSocket serverSocket) {
+    public ServidorThread(ServerSocket serverSocket, Socket socket) {
         this.socketServidor = serverSocket;
+        this.controlPartida = new ControlPartida();
+        ClientHandler clientHandler = new ClientHandler(socket, controlPartida);
+        Thread thread = new Thread(clientHandler);
+        thread.start();
     }
     
-        public void arrancarServidor(){
+    public void arrancarServidor(){
         try {
-            while (true){
+            while (!controlPartida.partidaLlena() && !controlPartida.partidaEmpezada()){
                 Socket socket = socketServidor.accept();
-                ServidorThread servidorThread = new ServidorThread(new ServerSocket(0),socket);
-                Thread thread = new Thread(servidorThread);
+                ClientHandler clientHandler = new ClientHandler(socket,controlPartida);
+                Thread thread = new Thread(clientHandler);
                 thread.start();
+            }
+            cerrarSocketServidor();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    private void cerrarSocketServidor(){
+        try {
+            if (socketServidor != null) {
+                socketServidor.close();
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    @Override
+    public void run() {
+        arrancarServidor();
     }
 }
